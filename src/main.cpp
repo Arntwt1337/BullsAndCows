dac7a14e3dc71c95591f88799def6cf6a1c083c4
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-09-30 21:39:11 +0700

    Write menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 
-int main(){
-}
+int main() {
+  int choose;
+  while (true) {
+    std::cin >> choose;
+    switch (choose) {
+    case 0:
+      exit(0);
+      break;
+    case 1:
+      system("clear");
+      break;
+    default:
+      std::cout << "Enter 0 or 1";
+    }
+  }
