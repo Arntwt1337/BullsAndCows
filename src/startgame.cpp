57c8d38d53c280b1bdbd744469c6cf6cb87b52ba
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-09-30 21:52:11 +0700

    Write startgame.cpp

---
+++ src/startgame.cpp
@@ -0,0 +1,12 @@
+#include "func.h"
+#include <iostream>
+
+void start_game() {
+  std::cout << "Game started";
+  srand(time(NULL));
+  int ran[4];
+  for (int temp = 0; temp < 4; temp++) {
+    ran[temp] = rand() % 10;
+    std::cout << ran[temp];
+  }
+}
