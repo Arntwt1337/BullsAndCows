15d45dd3ad98a311ecbc6c95ae262754573e698f
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-09-30 22:09:47 +0700

    Write maingame.cpp

--- src/maingame.cpp
+++ src/maingame.cpp
@@ -3,4 +3,14 @@
 #include <iostream>
 
 int game(int *ran) {
+  int c[10], a, b; //c - ваше слово
+  while (true) {
+    std::cin >> a;
+    for (int i = 3; i >= 0; i--) {
+      b = a % 10;
+      c[i] = b;
+      a = (a - b) / 10;
+    }
+  }
+  return 0;
 }
