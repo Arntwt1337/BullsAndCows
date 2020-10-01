dcf27805b712d0140373507a4216606487db990b
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-10-01 21:51:48 +0700

    Write test

--- test/test.cpp
+++ test/test.cpp
@@ -34,3 +34,48 @@ TEST(BullsTest, TwoRight) {
   int c[] = {1, 1, 1, 1};
   EXPECT_EQ(bulls_calc(ran, c), 2);
 }
+
+TEST(BullsTest, ThreeRight) {
+  srand(time(NULL));
+  int ran[] = {1, 1, 1, 0};
+  int c[] = {1, 1, 1, 1};
+  EXPECT_EQ(bulls_calc(ran, c), 3);
+}
+
+TEST(CowTest, EqTest) {
+  srand(time(NULL));
+  int ran[4], c[4];
+  for (int j = 0; j < 4; j++) {
+    ran[j] = rand();
+    c[j] = ran[j];
+  }
+  EXPECT_EQ(cows_calc(ran, c), 4);
+}
+
+TEST(CowTest, UnEqTest) {
+  srand(time(NULL));
+  int ran[] = {0, 0, 0, 0};
+  int c[] = {1, 1, 1, 1};
+  EXPECT_EQ(cows_calc(ran, c), 4);
+}
+
+TEST(CowsTest, OneRight) {
+  srand(time(NULL));
+  int ran[] = {1, 0, 0, 0};
+  int c[] = {1, 1, 1, 1};
+  EXPECT_EQ(cows_calc(ran, c), 1);
+}
+
+TEST(CowsTest, TwoRight) {
+  srand(time(NULL));
+  int ran[] = {1, 1, 0, 0};
+  int c[] = {1, 1, 1, 1};
+  EXPECT_EQ(cows_calc(ran, c), 2);
+}
+
+TEST(CowsTest, ThreeRight) {
+  srand(time(NULL));
+  int ran[] = {1, 1, 1, 0};
+  int c[] = {1, 1, 1, 1};
+  EXPECT_EQ(cows_calc(ran, c), 3);
+}
