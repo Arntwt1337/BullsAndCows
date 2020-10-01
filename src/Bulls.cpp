ad55283d3adc5f92ff4233c60bed35a42bc6bf64
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-10-01 16:40:05 +0700

    Update Bulls.cpp

--- src/Bulls.cpp
+++ src/Bulls.cpp
@@ -1,2 +1,8 @@
-int game(int *ran);
-void start_game();
+int bulls_calc(int *ran, int *c, int bull) { //Считаем быков
+  for (int i = 0; i < 4; i++) {
+    if (c[i] == ran[i]) {
+      bull++;
+    }
+  }
+  return bull;
+}
