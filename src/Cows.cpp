543d0e6382e67bae5c5fbaecb1eaffed3c9eb246
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-10-01 22:06:42 +0700

    Fix bug

--- src/Cows.cpp
+++ src/Cows.cpp
@@ -1,9 +1,11 @@
 int cows_calc(int *ran, int *c) { //Считаем коров
   int cow = 0;
   for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      if (c[i] == ran[j]) {
-        cow++;
+    if (c[i] != c[i - 1]) {
+      for (int j = 0; j < 4; j++) {
+        if (c[i] == ran[j]) {
+          cow++;
+        }
       }
     }
   }
