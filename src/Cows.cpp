bf9c3463a149e1375642042b9612557396d96a99
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-10-01 16:40:41 +0700

    Update Cows.cpp

--- src/Cows.cpp
+++ src/Cows.cpp
@@ -1,2 +1,10 @@
-int game(int *ran);
-void start_game();
+int cows_calc(int *ran, int *c, int cow) { //Считаем коров
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (c[i] == ran[j]) {
+        cow++;
+      }
+    }
+  }
+  return cow;
+}
