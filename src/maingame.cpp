140e790cdcc361e7536bd40cd4bff48cc7b48bdd
Ignat Hlavnovich <israel-10w89@mail.ru>
2020-10-01 17:10:11 +0700

    Fix bug

--- src/maingame.cpp
+++ src/maingame.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 
 int game(int *ran) {
-  int c[10], a, b, s; // c - ваше слово
-  step = 0;
+  int c[10], a, b, step = 0; // c - ваше слово
   while (true) {
     int cow = 0;
     int bull = 0;
@@ -13,13 +12,13 @@ int game(int *ran) {
     if (a == 0) {
       return 0;
     }
-    for (int i = 3; i >= 0; i--) {
+    for (int i = 3; i >= 0; i--) { // преобразуем а в ваше слово
       b = a % 10;
       c[i] = b;
-      a = (a - b) / 10;
+      a = (a - b) / 10; // разделяем слово на части
     }
-    bull = bulls_calc(ran, c, bull);
-    cow = bulls_calc(ran, c, cow);
+    bull = bulls_calc(ran, c, bull); // считаем быков
+    cow = bulls_calc(ran, c, cow); // считаем коров
     std::cout << step << ": " << bull << " - bulls   " << cow - bull << " - cows\n";
     if (bull == 4) {
       return 1;
